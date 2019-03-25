c6319c137701e95640bf008abfdf06b33a182c2e
Lasse Henriksen <lass5676@edu.easj.dk>
2019-03-25 01:39:34 +0100

    Liste af kaffe vises nu også i konsol. Opgave 1-7 lavet.

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,20 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+import { cortado } from "./cortado";
+import { latte } from "./latte";
+import { kaffe } from "./kaffe";
+import { sortkaffe } from "./sortkaffe";
+
+let kaffelist:kaffe[] = new Array();
+kaffelist.push(new latte(0));
+kaffelist.push(new sortkaffe(5));
+kaffelist.push(new cortado(0));
+kaffelist.forEach(k => printFunc(k))
+
+function printFunc(k : kaffe) {
+    console.log("Type: ", k.kaffeType);
+    console.log("Styrke: ", k.styrke);
+    console.log("Pris: ", k.pris)
+    console.log("Rabat: ", k.Rabat)
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+
